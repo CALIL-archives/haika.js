4454ba1ac19a0de1a4e82201ebca2f5dd422ff55
Satoshi Deguchi <deguchik@gmail.com>
2014-09-19 11:20:55 +0900

    mapをリファクタ

--- js/haika-map.coffee
+++ js/haika-map.coffee
@@ -1,121 +1,124 @@
+# テスト ロード時にマップ表示
 #$ ->
 #  setTimeout ->
 #    $($('.map_setting')[0]).trigger('click')
 #  , 1000
 
-map_created = false
-$('.map_setting').click ->
-  if $('.haika_container').css('display')=='block'
-    if not map_created
-      map_setting()
-      map_created = true
-    $('.haika_container').hide()
-    $(document.body).css('background', '#333333')
-    map_redraw()
-    $('.map_container').show()
-    $('#map_query').focus()
-  else
-    $(document.body).css('background', '#FFFFFF')
-    $('.haika_container').show()
-    $('.map_container').hide()
-
-map = null
-features = []
-map_redraw = ()->
-  if features.length>0
-    for feature in features
-        map.data.remove feature
-      features = map.data.addGeoJson(haika.createGeoJson())
-
-map_set = (lat, lon)->
-  $('#canvas_lon').val(lon)
-  $('#canvas_lat').val(lat)
-  haika.options.lon = lon
-  haika.options.lat = lat
-  haika.save()
-
-log haika.options.angle
-map_setting = ->
-  log haika.options.angle
-
-  map = new google.maps.Map(document.getElementById('map'),
-    zoom: 20
-    maxZoom: 28
-    center:
-      lat: haika.options.lat
-      lng: haika.options.lon
-  )
-  # Set the stroke width, and fill color for each polygon
-  featureStyle = {
-    fillColor: 'orange',
-    strokeWeight: 1
-  }
-  map.data.setStyle(featureStyle)
-  features = map.data.addGeoJson(haika.createGeoJson())
-  
-  google.maps.event.addListener map, 'dragend', ->
-    log map.getCenter()
-    lon = map.getCenter().lng()
-    lat = map.getCenter().lat()
-    map_set(lat, lon)
-    map_redraw()
-
-  $('#map_search').submit ->
-#    alert $('#map_query').val()
-    address = $('#map_query').val()
-    geocoder = new google.maps.Geocoder()
-    geocoder.geocode
-      address: address
-    , (results, status) ->
-      if status==google.maps.GeocoderStatus.OK
-        map.setCenter results[0].geometry.location
-  #        marker = new google.maps.Marker(
-  #          map: map
-  #          position: results[0].geometry.location
-  #        )
-        map_set(results[0].geometry.location.lat(), results[0].geometry.location.lng())
-        map_redraw()
-      else
-        alert "ジオコーディングがうまくいきませんでした。: " + status
-    return false
-
-  $('#canvas_lat').change ->
-    haika.options.lat = parseFloat($(this).val())
-    haika.save()
-  $('#canvas_lon').change ->
-    haika.options.lon = parseFloat($(this).val())
-    haika.save()
-
-  $('#canvas_angle').change ->
-    map_redraw()
-
-  $('#canvas_angle').slider
-    tooltip: 'always'
-    step: 1
-    min: 0
-    max: 360
-    value: haika.options.angle
-    formatter: (value) ->
-      haika.options.angle = parseFloat(value)
+# haikaの地図設定
+# haikaを拡張
+$.extend haika, 
+  map:
+    map : null
+    created : false
+    features : []
+    init : ->
+      $('.map_setting').click =>
+        if $('.haika_container').css('display')=='block'
+          if not @created
+            @set()
+            @created = true
+          $('.haika_container').hide()
+          $(document.body).css('background', '#333333')
+          @redraw()
+          $('.map_container').show()
+          $('#map_query').focus()
+        else
+          $(document.body).css('background', '#FFFFFF')
+          $('.haika_container').show()
+          $('.map_container').hide()
+    redraw : ->
+      if @features.length>0
+        for feature in @features
+            @map.data.remove feature
+          @features = @map.data.addGeoJson(haika.createGeoJson())
+    save : (lat, lon)->
+      $('#canvas_lon').val(lon)
+      $('#canvas_lat').val(lat)
+      haika.options.lon = lon
+      haika.options.lat = lat
       haika.save()
-      map_redraw()
-      return value+'度'
+    set : ->
+      @map = new google.maps.Map(document.getElementById('map'),
+        zoom: 20
+        maxZoom: 28
+        center:
+          lat: haika.options.lat
+          lng: haika.options.lon
+      )
+      # Set the stroke width, and fill color for each polygon
+      featureStyle = {
+        fillColor: 'orange',
+        strokeWeight: 1
+      }
+      @map.data.setStyle(featureStyle)
+      @features = @map.data.addGeoJson(haika.createGeoJson())
 
-$('#geojson_scale').slider
-    tooltip: 'always'
-    step: 1
-    min: 0
-    max: 400
-    value: haika.options.geojson_scale * 100
-    formatter: (value) ->
-      haika.options.geojson_scale = parseFloat(value) / 100
-      haika.save()
-      map_redraw()
-      return value+'%'
+      google.maps.event.addListener @map, 'dragend', =>
+        log @map.getCenter()
+        lon = @map.getCenter().lng()
+        lat = @map.getCenter().lat()
+        @save(lat, lon)
+        @redraw()
+
+      $('#map_search').submit =>
+    #    alert $('#map_query').val()
+        address = $('#map_query').val()
+        geocoder = new google.maps.Geocoder()
+        geocoder.geocode
+          address: address
+        , (results, status) =>
+          if status==google.maps.GeocoderStatus.OK
+            @map.setCenter results[0].geometry.location
+      #        marker = new google.maps.Marker(
+      #          map: @map.map
+      #          position: results[0].geometry.location
+      #        )
+            @save(results[0].geometry.location.lat(), results[0].geometry.location.lng())
+            @redraw()
+          else
+            alert "ジオコーディングがうまくいきませんでした。: " + status
+        return false
+
+      $('#canvas_lat').change ->
+        haika.options.lat = parseFloat($(this).val())
+        haika.save()
+      $('#canvas_lon').change ->
+        haika.options.lon = parseFloat($(this).val())
+        haika.save()
+
+      $('#canvas_angle').change =>
+        @redraw()
+
+      $('#canvas_angle').slider
+        tooltip: 'always'
+        step: 1
+        min: 0
+        max: 360
+        value: haika.options.angle
+        formatter: (value) =>
+          haika.options.angle = parseFloat(value)
+          haika.save()
+          @redraw()
+          return value+'度'
+
+      $('#geojson_scale').slider
+          tooltip: 'always'
+          step: 1
+          min: 0
+          max: 400
+          value: haika.options.geojson_scale * 100
+          formatter: (value) =>
+            haika.options.geojson_scale = parseFloat(value) / 100
+            haika.save()
+            @redraw()
+            return value+'%'
+
+# 初期設定
+haika.map.init()
 
 
 #  if haika.isLocal()
-#    gmap.data.addGeoJson(haika.createGeoJson())
+#    map.data.addGeoJson(haika.createGeoJson())
 #  else
-#    gmap.data.loadGeoJson('/haika_store/data/'+sprintf('%06d',haika.id)+'.geojson')
+#    map.data.loadGeoJson('/haika_store/data/'+sprintf('%06d',haika.id)+'.geojson')
   
--- js/haika.all.js
+++ js/haika.all.js
@@ -26882,133 +26882,142 @@ t("ol.style.Image",Li);Li.prototype.getRotation=Li.prototype.g;Li.prototype.getS
 Oi.prototype.getText=Oi.prototype.rh;Oi.prototype.getZIndex=Oi.prototype.Af;t("ol.style.Text",Vv);Vv.prototype.getFill=Vv.prototype.sh;Vv.prototype.getFont=Vv.prototype.ff;Vv.prototype.getRotation=Vv.prototype.th;Vv.prototype.getScale=Vv.prototype.uh;Vv.prototype.getStroke=Vv.prototype.vh;Vv.prototype.getText=Vv.prototype.wh;Vv.prototype.getTextAlign=Vv.prototype.vf;Vv.prototype.getTextBaseline=Vv.prototype.wf;t("ol.tilegrid.TileGrid",Hk);Hk.prototype.getMinZoom=Hk.prototype.Hc;
 Hk.prototype.getOrigin=Hk.prototype.lb;Hk.prototype.getResolution=Hk.prototype.ca;Hk.prototype.getTileSize=Hk.prototype.ia;t("ol.tilegrid.WMTS",Nv);Nv.prototype.getMatrixIds=Nv.prototype.i;Nv.prototype.getMinZoom=Nv.prototype.Hc;Nv.prototype.getOrigin=Nv.prototype.lb;Nv.prototype.getResolution=Nv.prototype.ca;Nv.prototype.getTileSize=Nv.prototype.ia;t("ol.tilegrid.XYZ",Bu);Bu.prototype.getMinZoom=Bu.prototype.Hc;Bu.prototype.getOrigin=Bu.prototype.lb;Bu.prototype.getResolution=Bu.prototype.ca;
 Bu.prototype.getTileSize=Bu.prototype.ia;t("ol.tilegrid.Zoomify",Sv);Sv.prototype.getMinZoom=Sv.prototype.Hc;Sv.prototype.getOrigin=Sv.prototype.lb;Sv.prototype.getResolution=Sv.prototype.ca;Sv.prototype.getTileSize=Sv.prototype.ia;t("ol.webgl.Context",Jn);Jn.prototype.getGL=Jn.prototype.xh;Jn.prototype.useProgram=Jn.prototype.wd;})();
-;var features, map, map_created, map_redraw, map_set, map_setting;
-
-map_created = false;
-
-$('.map_setting').click(function() {
-  if ($('.haika_container').css('display') === 'block') {
-    if (!map_created) {
-      map_setting();
-      map_created = true;
-    }
-    $('.haika_container').hide();
-    $(document.body).css('background', '#333333');
-    map_redraw();
-    $('.map_container').show();
-    return $('#map_query').focus();
-  } else {
-    $(document.body).css('background', '#FFFFFF');
-    $('.haika_container').show();
-    return $('.map_container').hide();
-  }
-});
-
-map = null;
-
-features = [];
-
-map_redraw = function() {
-  var feature, _i, _len;
-  if (features.length > 0) {
-    for (_i = 0, _len = features.length; _i < _len; _i++) {
-      feature = features[_i];
-      map.data.remove(feature);
-    }
-    return features = map.data.addGeoJson(haika.createGeoJson());
-  }
-};
-
-map_set = function(lat, lon) {
-  $('#canvas_lon').val(lon);
-  $('#canvas_lat').val(lat);
-  haika.options.lon = lon;
-  haika.options.lat = lat;
-  return haika.save();
-};
-
-log(haika.options.angle);
-
-map_setting = function() {
-  var featureStyle;
-  log(haika.options.angle);
-  map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 20,
-    maxZoom: 28,
-    center: {
-      lat: haika.options.lat,
-      lng: haika.options.lon
-    }
-  });
-  featureStyle = {
-    fillColor: 'orange',
-    strokeWeight: 1
-  };
-  map.data.setStyle(featureStyle);
-  features = map.data.addGeoJson(haika.createGeoJson());
-  google.maps.event.addListener(map, 'dragend', function() {
-    var lat, lon;
-    log(map.getCenter());
-    lon = map.getCenter().lng();
-    lat = map.getCenter().lat();
-    map_set(lat, lon);
-    return map_redraw();
-  });
-  $('#map_search').submit(function() {
-    var address, geocoder;
-    address = $('#map_query').val();
-    geocoder = new google.maps.Geocoder();
-    geocoder.geocode({
-      address: address
-    }, function(results, status) {
-      if (status === google.maps.GeocoderStatus.OK) {
-        map.setCenter(results[0].geometry.location);
-        map_set(results[0].geometry.location.lat(), results[0].geometry.location.lng());
-        return map_redraw();
-      } else {
-        return alert("ジオコーディングがうまくいきませんでした。: " + status);
+;$.extend(haika, {
+  map: {
+    map: null,
+    created: false,
+    features: [],
+    init: function() {
+      return $('.map_setting').click((function(_this) {
+        return function() {
+          if ($('.haika_container').css('display') === 'block') {
+            if (!_this.created) {
+              _this.set();
+              _this.created = true;
+            }
+            $('.haika_container').hide();
+            $(document.body).css('background', '#333333');
+            _this.redraw();
+            $('.map_container').show();
+            return $('#map_query').focus();
+          } else {
+            $(document.body).css('background', '#FFFFFF');
+            $('.haika_container').show();
+            return $('.map_container').hide();
+          }
+        };
+      })(this));
+    },
+    redraw: function() {
+      var feature, _i, _len, _ref;
+      if (this.features.length > 0) {
+        _ref = this.features;
+        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+          feature = _ref[_i];
+          this.map.data.remove(feature);
+        }
+        return this.features = this.map.data.addGeoJson(haika.createGeoJson());
       }
-    });
-    return false;
-  });
-  $('#canvas_lat').change(function() {
-    haika.options.lat = parseFloat($(this).val());
-    return haika.save();
-  });
-  $('#canvas_lon').change(function() {
-    haika.options.lon = parseFloat($(this).val());
-    return haika.save();
-  });
-  $('#canvas_angle').change(function() {
-    return map_redraw();
-  });
-  return $('#canvas_angle').slider({
-    tooltip: 'always',
-    step: 1,
-    min: 0,
-    max: 360,
-    value: haika.options.angle,
-    formatter: function(value) {
-      haika.options.angle = parseFloat(value);
-      haika.save();
-      map_redraw();
-      return value + '度';
+    },
+    save: function(lat, lon) {
+      $('#canvas_lon').val(lon);
+      $('#canvas_lat').val(lat);
+      haika.options.lon = lon;
+      haika.options.lat = lat;
+      return haika.save();
+    },
+    set: function() {
+      var featureStyle;
+      this.map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 20,
+        maxZoom: 28,
+        center: {
+          lat: haika.options.lat,
+          lng: haika.options.lon
+        }
+      });
+      featureStyle = {
+        fillColor: 'orange',
+        strokeWeight: 1
+      };
+      this.map.data.setStyle(featureStyle);
+      this.features = this.map.data.addGeoJson(haika.createGeoJson());
+      google.maps.event.addListener(this.map, 'dragend', (function(_this) {
+        return function() {
+          var lat, lon;
+          log(_this.map.getCenter());
+          lon = _this.map.getCenter().lng();
+          lat = _this.map.getCenter().lat();
+          _this.save(lat, lon);
+          return _this.redraw();
+        };
+      })(this));
+      $('#map_search').submit((function(_this) {
+        return function() {
+          var address, geocoder;
+          address = $('#map_query').val();
+          geocoder = new google.maps.Geocoder();
+          geocoder.geocode({
+            address: address
+          }, function(results, status) {
+            if (status === google.maps.GeocoderStatus.OK) {
+              _this.map.setCenter(results[0].geometry.location);
+              _this.save(results[0].geometry.location.lat(), results[0].geometry.location.lng());
+              return _this.redraw();
+            } else {
+              return alert("ジオコーディングがうまくいきませんでした。: " + status);
+            }
+          });
+          return false;
+        };
+      })(this));
+      $('#canvas_lat').change(function() {
+        haika.options.lat = parseFloat($(this).val());
+        return haika.save();
+      });
+      $('#canvas_lon').change(function() {
+        haika.options.lon = parseFloat($(this).val());
+        return haika.save();
+      });
+      $('#canvas_angle').change((function(_this) {
+        return function() {
+          return _this.redraw();
+        };
+      })(this));
+      $('#canvas_angle').slider({
+        tooltip: 'always',
+        step: 1,
+        min: 0,
+        max: 360,
+        value: haika.options.angle,
+        formatter: (function(_this) {
+          return function(value) {
+            haika.options.angle = parseFloat(value);
+            haika.save();
+            _this.redraw();
+            return value + '度';
+          };
+        })(this)
+      });
+      return $('#geojson_scale').slider({
+        tooltip: 'always',
+        step: 1,
+        min: 0,
+        max: 400,
+        value: haika.options.geojson_scale * 100,
+        formatter: (function(_this) {
+          return function(value) {
+            haika.options.geojson_scale = parseFloat(value) / 100;
+            haika.save();
+            _this.redraw();
+            return value + '%';
+          };
+        })(this)
+      });
     }
-  });
-};
-
-$('#geojson_scale').slider({
-  tooltip: 'always',
-  step: 1,
-  min: 0,
-  max: 400,
-  value: haika.options.geojson_scale * 100,
-  formatter: function(value) {
-    haika.options.geojson_scale = parseFloat(value) / 100;
-    haika.save();
-    map_redraw();
-    return value + '%';
   }
 });
+
+haika.map.init();
 
 //# sourceMappingURL=haika-map.js.map
