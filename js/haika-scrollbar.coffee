0cea3e289463124d5f3f544b67a69532dcab7391
Ryuuji Yoshimoto <ryuuji.y@gmail.com>
2014-10-02 16:06:20 +0900

    スクロールの変更

--- js/haika-scrollbar.coffee
+++ js/haika-scrollbar.coffee
@@ -1,35 +1,18 @@
 # スクロールバーの設定
-# haikaの初期設定完了時に実行する  
+# haikaの初期設定完了時に実行する
 initScrollBar = () ->
-  scroll_weight = 5000
-  bgimg_width = if haika.backgroundImage then haika.backgroundImage.width else 2500
-  bgimg_height = if haika.backgroundImage then haika.backgroundImage.height else 2500
-  maxX = bgimg_width * haika.backgroundScaleFactor / 2
-  maxY = bgimg_height * haika.backgroundScaleFactor / 2
-  defaultX =  -((haika.centerX - scroll_weight) / 10000)
-  defaultY =  -((haika.centerY - scroll_weight) / 10000)
   new Dragdealer 'horizontal-scroller',
-    x: defaultX
+    x: 0.5
     animationCallback: (x, y)->
-#      log x
-      centerX = x * 10000 - scroll_weight * haika.scaleFactor
-      if centerX > maxX - haika.canvas.getWidth() / 2
-        centerX = maxX - haika.canvas.getWidth() / 2
-      if centerX < -maxX + haika.canvas.getWidth() / 2
-        centerX = -maxX + haika.canvas.getWidth() / 2
-      haika.centerX = -centerX.toFixed(0)
+      _max= if haika.canvas.backgroundImage then haika.canvas.backgroundImage.width else 2500
+      haika.centerX = ((x-0.5)*_max).toFixed(0)*-1
       haika.render()
   new Dragdealer 'vertical-scroller',
-    y: defaultY
+    y: 0.5
     horizontal: false,
     vertical: true,
-#    yPrecision: 500,
     animationCallback: (x, y)->
-      centerY = y * 10000 - scroll_weight * haika.scaleFactor
-      if centerY > maxY - haika.canvas.getHeight() / 2
-        centerY = maxY - haika.canvas.getHeight() / 2
-      if centerY < -maxY + haika.canvas.getHeight() / 2
-        centerY = -maxY + haika.canvas.getHeight() / 2
-      haika.centerY = -centerY.toFixed(0)
+      _max= if haika.canvas.backgroundImage then haika.canvas.backgroundImage.height else 2500
+      haika.centerY = ((y-0.5)*_max).toFixed(0)*-1
       haika.render()
 
--- js/haika-scrollbar.js
+++ js/haika-scrollbar.js
@@ -2,43 +2,23 @@
 var initScrollBar;
 
 initScrollBar = function() {
-  var bgimg_height, bgimg_width, defaultX, defaultY, maxX, maxY, scroll_weight;
-  scroll_weight = 5000;
-  bgimg_width = haika.backgroundImage ? haika.backgroundImage.width : 2500;
-  bgimg_height = haika.backgroundImage ? haika.backgroundImage.height : 2500;
-  maxX = bgimg_width * haika.backgroundScaleFactor / 2;
-  maxY = bgimg_height * haika.backgroundScaleFactor / 2;
-  defaultX = -((haika.centerX - scroll_weight) / 10000);
-  defaultY = -((haika.centerY - scroll_weight) / 10000);
   new Dragdealer('horizontal-scroller', {
-    x: defaultX,
+    x: 0.5,
     animationCallback: function(x, y) {
-      var centerX;
-      centerX = x * 10000 - scroll_weight * haika.scaleFactor;
-      if (centerX > maxX - haika.canvas.getWidth() / 2) {
-        centerX = maxX - haika.canvas.getWidth() / 2;
-      }
-      if (centerX < -maxX + haika.canvas.getWidth() / 2) {
-        centerX = -maxX + haika.canvas.getWidth() / 2;
-      }
-      haika.centerX = -centerX.toFixed(0);
+      var _max;
+      _max = haika.canvas.backgroundImage ? haika.canvas.backgroundImage.width : 2500;
+      haika.centerX = ((x - 0.5) * _max).toFixed(0) * -1;
       return haika.render();
     }
   });
   return new Dragdealer('vertical-scroller', {
-    y: defaultY,
+    y: 0.5,
     horizontal: false,
     vertical: true,
     animationCallback: function(x, y) {
-      var centerY;
-      centerY = y * 10000 - scroll_weight * haika.scaleFactor;
-      if (centerY > maxY - haika.canvas.getHeight() / 2) {
-        centerY = maxY - haika.canvas.getHeight() / 2;
-      }
-      if (centerY < -maxY + haika.canvas.getHeight() / 2) {
-        centerY = -maxY + haika.canvas.getHeight() / 2;
-      }
-      haika.centerY = -centerY.toFixed(0);
+      var _max;
+      _max = haika.canvas.backgroundImage ? haika.canvas.backgroundImage.height : 2500;
+      haika.centerY = ((y - 0.5) * _max).toFixed(0) * -1;
       return haika.render();
     }
   });
